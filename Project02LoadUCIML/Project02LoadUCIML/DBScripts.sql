25f9548b5116d2fc0d3fcab0e5d6f74a85d2774d
MR <m.rzechowicz@gmail.com>
2019-05-05 22:21:04 +0200

    zadanie2

--- Project02LoadUCIML/Project02LoadUCIML/DBScripts.sql
+++ Project02LoadUCIML/Project02LoadUCIML/DBScripts.sql
@@ -1,17 +1,20 @@
-CREATE TABLE [Abalone] (
-    [Sex] varchar(1),
-    [Length] numeric(10,5),
-    [Diameter] numeric(10,5),
-    [Height] numeric(10,5),
-    [WholeWeight] numeric(10,5),
-    [ShuckedWeight] numeric(10,5),
-    [VisceraWeight] numeric(10,5),
-    [ShellWeight] numeric(10,5),
-    [Rings] int
-)
+if not exists (select * from sysobjects where name='Abalone' and xtype='U')
+	CREATE TABLE [Abalone] (
+		[Sex] varchar(1),
+		[Length] numeric(10,5),
+		[Diameter] numeric(10,5),
+		[Height] numeric(10,5),
+		[WholeWeight] numeric(10,5),
+		[ShuckedWeight] numeric(10,5),
+		[VisceraWeight] numeric(10,5),
+		[ShellWeight] numeric(10,5),
+		[Rings] int
+	)
+go
 
 
-CREATE TABLE [Adult] (
+if not exists (select * from sysobjects where name='Adult' and xtype='U')
+	CREATE TABLE [Adult] (
     [age] int,
     [workclass] varchar(40),
     [fnlwgt] int,
@@ -28,3 +31,12 @@ CREATE TABLE [Adult] (
     [native-country] varchar(40),
     [city-population] varchar(50)
 )
+go
+
+if not exists (select * from sysobjects where name='PackageLogs' and xtype='U')
+CREATE TABLE [PackageLogs] (
+	[StartDate] datetime,
+	[StartedBy] varchar(100),
+	[PackageName] varchar(100)
+)
+go
